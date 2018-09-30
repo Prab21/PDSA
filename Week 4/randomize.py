8bacfdef9328faf2eaa4ee539e78213b05e11bc2
unknown <scindia.harsh@gmail.com>
2018-09-30 22:55:01 +0530

    'randomize.py'

--- Week 4/randomize.py
+++ Week 4/randomize.py
@@ -1,1 +1,6 @@
 import random
+def randomize(l):
+    for i in range(len(l)//2):
+        j = random.randomrange(0, len(l), 1)
+        k = random.randomrange(0, len(l), 1)
+        (l[j], l[k]) = (l[k], l[j])
