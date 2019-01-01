d8775c2ae3ed4b8b268b080808560bdd185fa4d0
unknown <scindia.harsh@gmail.com>
2019-01-01 19:22:56 +0530

    added function copyright()

--- Week 7/search_tree.py
+++ Week 7/search_tree.py
@@ -1,2 +1,36 @@
 class Tree:
     # Empty node has self.value, self.left, self.right = None
+    # Leaf has self.value != None, and self.left, self.right point to empty node
+
+    # Constructor: create an empty node or a leaf node, depending on initval
+    def __init__(self, initval=None):
+        self.value = initval
+        if self.value:
+            self.left = Tree()
+            self.right = Tree()
+        else:
+            self.left = None
+            self.right = None
+        return
+
+    # Only empty node has value None
+    def isempty(self):
+        return (self.value == None)
+
+    # Leaf nodes have both children empty
+    def isleaf(self):
+        return (self.left.isempty() and self.right.isempty())
+
+    # Convert a leaf node to an empty node
+    def makeempty(self):
+        self.value = None
+        self.left = None
+        self.right = None
+        return
+    
+    # Copy right child values to current node
+    def copyright(self):
+        self.value = self.right.value
+        self.left = self.right.left
+        self.right = self.right.right
+        return
